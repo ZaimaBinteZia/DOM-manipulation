8b42644497b3a69efb694ad7dc9103e2d2f97dd2
Zaimabintezia <Zaimazia68@gmail.com>
2024-12-13 12:29:22 +0600

    DOM of HTML done.

---
+++ script.js
@@ -0,0 +1,21 @@
+let div = document.createElement('div');
+div.id = 'paragraph';
+div.innerHTML = '<p>I am the created div!</p>';
+document.body.append(div);
+let heading = document.getElementById('heading');
+heading.innerHTML = '&#128512;';
+// Emoji Codes
+128513, 128514, 128515, 128516;
+
+document.querySelector('header p').innerHTML = '<strong>The paragraph in JS!</strong>';
+
+document.body.style.backgroundColor = 'pink';
+
+let image = document.getElementById('image')
+image.src = 'js image.jpeg';
+image.style.display = 'block';
+
+let div2 = document.createElement('div');
+div2.innerHTML = '<p>I am the second div!!</p>';
+div2.style.color = 'white';
+document.body.append(div2);
